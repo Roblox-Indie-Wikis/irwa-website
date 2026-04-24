cd315f89ea8f61c63dbd3432d2436260bea07e17
whostacking <lostfromthelabyrinth@gmail.com>
2026-04-25 00:45:45 +0800

    testing again

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -828,7 +828,3 @@ if (document.readyState === 'loading') {
 } else {
 	initHomeBackgroundRotation();
 }
-
-if (window.location.pathname.endsWith('/') && window.location.pathname !== '/') {
-	window.location.replace(window.location.pathname.slice(0, -1));
-}
